7d6958fcc942c72b03ace5e57308becb9fe0d1f7
matizen <matizen2002@yandex.ru>
2022-05-23 01:01:37 +0300

    1.1

--- handlers/msg_handlers.py
+++ handlers/msg_handlers.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from loader import dp
+import anek
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -7,6 +8,13 @@ async def greeting(message: types.Message):
     await message.answer("Привет, теперь будешь уведомленя получать")
 
 
+@dp.message_handler(commands=['anecdote'])
+async def greeting(message: types.Message):
+    await message.delete()
+    await message.answer(anek.get_anek())
+
+
 @dp.message_handler()
 async def echo(message: types.Message):
     print(message.text)
+    await message.answer("я еще маленький бот, не умею грамотно отвечать, но могу /anecdote вкинуть)")
